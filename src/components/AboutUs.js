dd83e1b4167eb23576e1861503a9f07264f4b797
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-31 14:48:12 +0100

    Router component created

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        AboutUs
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
---
+++ src/components/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "./App";
+import AboutUs from "./AboutUs";
+import NotFound from "./NotFound";
+
+class Router extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+      	<Switch>
+      		<Route exact path="/" component={App}></Route>
+      		<Route exact path="/about-us" component={AboutUs}></Route>
+      		<Route component={NotFound}></Route>
+      	</Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Router;
