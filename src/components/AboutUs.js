68dafe23cdc8465d7019193dfccb61017c5ee25f
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-02 22:13:37 +0100

    AboutUs component styled

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 class AboutUs extends React.Component {
   render() {
     return (
       <div className="about-us">
-        AboutUs
+        <Logo />
+		<Navbar />
+		<div className="info-about-us">
+			<h3 className="company">Candy Shop
+			</h3>
+			<img src="#" alt="Our Company"/>
+			<p className="desc">
+			Brownie cake cupcake sugar plum soufflé tiramisu dragée I love. Gummies topping chupa chups. Cupcake tart icing gummies cotton candy fruitcake. Oat cake soufflé apple pie gummi bears cookie wafer cupcake chocolate cake powder.
+Tootsie roll I love halvah dragée donut muffin pudding cupcake. Danish bonbon bear claw jelly beans marzipan biscuit marzipan I love. Sesame snaps danish ice cream chocolate bar donut cotton candy carrot cake powder. I love icing toffee caramels I love jelly beans.
+			</p>
+		</div>
+		<Footer />
       </div>
     );
   }
