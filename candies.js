17f05d005329dd938b237d5408e137d7d5208414
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-03 17:02:20 +0100

    candies.js created

---
+++ candies.js
@@ -0,0 +1,37 @@
+const candies = {
+	chocolatePie: {
+		name: "Chocolate Pie",
+		desc: "Blabla",
+		img: "#"
+	},
+	cinamonBuns: {
+		name: "Cinamon Buns",
+		desc: "Blabla",
+		img: "#"
+	},
+	donuts: {
+		name: "Donuts",
+		desc: "Blabla",
+		img: "#"
+	},
+	gingerCake: {
+		name: "Ginger Cake",
+		desc: "Blabla",
+		img: "#"
+	},
+	mashmallows: {
+		name: "Marshmallows",
+		desc: "Blabla",
+		img: "#"
+	},
+	pumpkinBalls: {
+		name: "Pumpkin Balls",
+		desc: "Blabla",
+		img: "#"
+	},
+	waffels: {
+		name: "Waffels",
+		desc: "Blabla",
+		img: "#"
+	}
+}
