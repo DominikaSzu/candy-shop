b4a5e00f2fe4515cac4cb4b33225b4397054e627
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-03 16:52:13 +0100

    CandyBasket component created

---
+++ src/components/CandyBasket.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CandyBasket extends React.Component {
+  render() {
+    return (
+      <div className="candy-basket">
+		<p>Elo</p>
+      </div>
+    );
+  }
+}
+
+export default CandyBasket;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Logo from "./Logo";
 import Footer from "./Footer";
+import CandyBasket from "./CandyBasket";
 
 class Cart extends React.Component {
   render() {
     return (
       <div className="cart">
 		<Logo />
+		<CandyBasket />
 		<Footer />
       </div>
     );
