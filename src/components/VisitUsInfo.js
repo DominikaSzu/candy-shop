26caddfa07aa311265891786bef2447b6aa2344a
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-10 21:04:52 +0100

    component VisitUsStyled

--- src/components/VisitUsInfo.js
+++ src/components/VisitUsInfo.js
@@ -5,7 +5,19 @@ class VisitUsInfo extends React.Component {
   render() {
     return (
       <div className="visit-us-info">
-        Elo Eloo
+        <p>Have you already decided, what you want to try
+        in our delicious Candy Shop? Great!</p>
+        <p>Come and visit us, here are the hours we will
+        welcome you with pleasure in our place:</p>
+        <div className="open-hours">
+          <span>Monday</span><span> 8 a.m. - 9 p.m.</span>
+          <span>Tuesday </span><span>8 a.m. - 9 p.m.</span>
+          <span>Wednesday </span><span>8 a.m. - 9 p.m.</span>
+          <span>Thursday </span><span>8 a.m. - 9 p.m.</span>
+          <span>Friday </span><span>8 a.m. - 9 p.m.</span>
+          <span>Saturday </span><span>8 a.m. - 9 p.m.</span>
+          <span>Sunday</span><span> 8 a.m. - 9 p.m. </span>
+        </div>
       </div>
     );
   }
