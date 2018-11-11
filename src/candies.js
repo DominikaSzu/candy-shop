4f50a1684d79636b75b3c5db6886866a805bbeac
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-11 22:16:05 +0100

    new candies description added

--- src/candies.js
+++ src/candies.js
@@ -1,38 +1,38 @@
 const candies = {
 	chocolatePie: {
 		name: "Chocolate Pie",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "What sense would have a candy shop without a chocolate pie? Try one piece and buy the whole cake. As our customers say, once you try it, there is no way back. ",
 		img: "/images/chocolatepie.jpg"
 	},
 	cinamonBuns: {
 		name: "Cinamon Buns",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "We have tried some of them in some Swedish furniture shop and we liked them so much, that we have decided to bake some by ourselves. Soft, tasty and made with love.",
 		img: "/images/cinamonbuns.jpg"
 	},
 	donuts: {
 		name: "Donuts",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "We weren't convinced if we should introduce them to our menu, but as we received many questions from the police, we have decided to do so. God, they're good.",
 		img: "/images/donuts.jpg"
 	},
 	gingerCake: {
 		name: "Ginger Cake",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "We should be modest, but seriously: this is our greatest stuff. We always prepare an extra portion for every employee, to make sure something will rest for our customers.",
 		img: "/images/gingercake.jpg"
 	},
 	mashmallows: {
 		name: "Marshmallows",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "Over the years, we have managed to elaborate our own, perfect marshmallow recipe. They are soft, gentle and tasty, like a piece of cloud in your mouth. Yummy.",
 		img: "/images/marshmallows.jpg"
 	},
 	pumpkinBalls: {
 		name: "Pumpkin Balls",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
+		desc: "You should definitely try our famous Pumpkin Balls, sweet and spicy at the same time. Even the hardest pumpkin opponents got obsessed with them. It's addictive.",
 		img: "/images/pumpkinballs.jpg"
 	},
 	waffels: {
-		name: "Waffels",
-		desc: "Cupcake ipsum dolor sit amet biscuit. Candy danish carrot cake toffee gummies brownie. Gummies halvah soufflé sweet roll liquorice cupcake caramels.",
-		img: "/images/waffels.jpg"
+		name: "Waffles",
+		desc: "When you hear \"Waffles\" you think that nothing can surprise you? Wrong! Our Waffles are unique. In 2017 we have won the global Waffle contest in Massachusetts.",
+		img: "/images/waffles.jpg"
 	}
 };
 
