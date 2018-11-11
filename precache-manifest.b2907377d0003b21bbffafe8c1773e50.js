dccc624ca647fe0cf3b6557759fd2a861634dc3e
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-11 22:57:13 +0100

    Updates

---
+++ precache-manifest.b2907377d0003b21bbffafe8c1773e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "039ff0810a7513f0b5c23e19a8298a10",
+    "url": "/static/media/company.039ff081.jpg"
+  },
+  {
+    "revision": "114f2dbffc5d137a1c9880a1e4e33a5d",
+    "url": "/static/media/coffee.114f2dbf.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23d5cf6d2f19b87638db",
+    "url": "/static/js/main.23d5cf6d.chunk.js"
+  },
+  {
+    "revision": "cf3a06fd0f4a4817251b",
+    "url": "/static/js/1.cf3a06fd.chunk.js"
+  },
+  {
+    "revision": "23d5cf6d2f19b87638db",
+    "url": "/static/css/main.66027abb.chunk.css"
+  },
+  {
+    "revision": "c1f9e53d03fe45c7f61bb740837171bf",
+    "url": "/index.html"
+  }
+];
