9499ccfd014051f22ad74f7b4450208e6e1e286e
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-11 22:32:45 +0100

    Styles added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     return (
       <div className="footer">
 		<span className="first-span" role="img" aria-label="Candy">Candy Shop 🍬</span>
-		<span className="second-span" role="img" aria-label="Heart">Me ♥</span>
+		<span className="second-span" role="img" aria-label="Heart"><a href="https://bit.ly/2z4jzzt">Me ♥</a></span>
       </div>
     );
   }
